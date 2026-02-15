ad8f0b6b3870b2ad364b59733cf3e1a533483186
Andreea Stoian <andreea@MacBook-Pro-Andreea.local>
2026-02-15 21:07:26 +0200

    Fail test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,3 +3,6 @@ from utils import calculate_total
 def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
+def test_calculate_total():
+    assert calculate_total([10, 15, 25]) == 45
+    assert calculate_total([]) == 0
